133bc4b315d0718580e04ba1dbfee08f16fdd2f5
Italo Sousa <italops94@gmail.com>
2021-03-06 17:58:39 -0300

    configuring pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>Boilerplate Next.JS</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#cbc3e3" />
         <meta
           name="description"
           content="Boilerplate to work with TypeScript, React and NextJS"
